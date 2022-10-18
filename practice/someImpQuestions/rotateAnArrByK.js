f6556b237c557a33be22f30883ec08ce861433ce
harsh-developer <harshpratap1444@gmail.com>
2022-10-18 23:19:15 +0530

    rotate array by k element is done

---
+++ practice/someImpQuestions/rotateAnArrByK.js
@@ -0,0 +1,22 @@
+// Rotate an array by K elements
+
+function rotateArrByK(arr, k){
+    arr = rotateArr(arr, 0, arr.length - 1)
+    // 5, 4, 3, 2, 1
+    let left = rotateArr(arr.slice(0, k), 0, k -1)
+    let right = rotateArr(arr.slice(k, arr.length), 0, arr.length - k - 1)
+    let result = left.concat(right)
+    return result;
+}
+
+function rotateArr(arr, start, end){
+    while(start < end){
+        arr[start] = arr[start] + arr[end];
+        arr[end] = arr[start] - arr[end];
+        arr[start] = arr[start] - arr[end];
+        start++;
+        end--
+    }
+    return arr;
+}
+console.log(rotateArrByK([1, 2, 3, 4, 5], 1))
