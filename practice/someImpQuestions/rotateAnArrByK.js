93b50807026ab13892999fa9e765048272cdbc06
harsh-developer <harshpratap1444@gmail.com>
2022-10-19 13:01:11 +0530

    handled an edge case in rotate array by K ele

--- practice/someImpQuestions/rotateAnArrByK.js
+++ practice/someImpQuestions/rotateAnArrByK.js
@@ -2,6 +2,8 @@
 
 function rotateArrByK(arr, k){
     arr = rotateArr(arr, 0, arr.length - 1)
+    // k%10 is for handling a case, when k's value is greater than the length of the array
+    k = k%10
     // 5, 4, 3, 2, 1
     let left = rotateArr(arr.slice(0, k), 0, k -1)
     let right = rotateArr(arr.slice(k, arr.length), 0, arr.length - k - 1)
@@ -19,4 +21,4 @@ function rotateArr(arr, start, end){
     }
     return arr;
 }
-console.log(rotateArrByK([1, 2, 3, 4, 5], 1))
+console.log(rotateArrByK([1, 2, 3, 4, 5], 11))
